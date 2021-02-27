edacd0a6f2be71e8175d802d99129dc66d995270
Billal Siddiqi <billalsiddiqi@yahoo.com>
2021-02-27 10:57:33 +0430

    Complate Success Modal

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+
 const body = document.getElementsByTagName('body')[0];
 const btnMobile = document.querySelector('#btnMobile');
 const over = document.querySelector('#over');
@@ -9,27 +10,31 @@ const openModal = document.querySelector('#open-modal');
 const closeModal = document.querySelector('#close-modal');
 const modal = document.querySelector('.back-modal');
 
+const bookmarkBtn = document.querySelector('.bookmark');
+const success = document.querySelector('.success-modal');
+const got = document.querySelector('.got');
 
 
+//Menu toggle
 btnMobile.addEventListener('click', ()=>{
     btnMobile.classList.toggle('open');
     over.classList.toggle('overlay');
     over.classList.toggle('overlay-animate');
-    over.classList.toggle('fade-out');
     nav.classList.toggle('has-fade');
     nav.classList.toggle('fade-in');
     body.classList.toggle('overflow-hidden')
 });
 
 
-
+//open modal
 openModal.addEventListener('click', ()=>{
     modal.classList.add('open-modal');
     modal.classList.add('fade-modal-in');
     over.classList.toggle('overlay');
     over.classList.toggle('overlay-modal');
+    
 });
-
+//close modal
 closeModal.addEventListener('click', ()=>{
     modal.classList.remove('open-modal');
     modal.classList.remove('fade-modal-in');
@@ -37,5 +42,25 @@ closeModal.addEventListener('click', ()=>{
     over.classList.toggle('overlay-modal');
 });
 
+//open success modal
+
+bookmarkBtn.addEventListener('click', ()=>{
+    success.classList.add('open-modal');
+    success.classList.add('fade-modal-in');
+    over.classList.toggle('overlay');
+    over.classList.toggle('overlay-success-modal');
+    body.classList.toggle('overflow-hidden');
+
+});
+
+// close success modal
+got.addEventListener('click', ()=>{
+    success.classList.remove('open-modal');
+    success.classList.remove('fade-modal-in');
+    over.classList.toggle('overlay');
+    over.classList.toggle('overlay-success-modal');
+    body.classList.toggle('overflow-hidden')
+
+})
 
 
